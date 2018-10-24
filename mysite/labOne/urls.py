167634b2b971b0951c8f36fe2aaf7a9dc418c1ef
K0NG6 <nazar.kislichenko@gmail.com>
2018-10-25 02:38:29 +0300

    Added method dyhotomia and Newton

--- mysite/labOne/urls.py
+++ mysite/labOne/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.forms, name='index'),
-    path('mplimage/', views.mplimage, name='forms')
+    path('mplimage/', views.mplimage, name='forms'),
+    path('dyhotomia/', views.dyhotomia, name='forms'),
+    path('newton/', views.dyhotomia, name='forms')
 ]
